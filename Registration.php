822ba3c376bb6e84ca9fcaa4bbe85463e0b0c5e2
Eddeez <141634996+Eddwie@users.noreply.github.com>
2024-07-10 11:52:18 +0300

    Add files via upload

--- Registration.php
+++ Registration.php
@@ -21,41 +21,15 @@
         </header>
 
         <div class="wrapper">
-            <span class="icon-close"><ion-icon name="close"></ion-icon></span>
-
-            <div class="form-box login">
-                <h2>Login</h2>
+            <span class="icon-close"><ion-icon name="close"></ion-icon></span>   
+            <div class="form-box register">
+                <h2>Registration</h2>
                 <form action="backend.php" method="post">
                     <select name = "user_type">
                         <option value="Director">Director</option>
                         <option value="Administtrator">Administrator</option>
                         <option value="Social_Worker">Social Worker</option>
                     </select>
-                    <div class="input-box">
-                        <span class="icon"><ion-icon name="mail"></ion-icon></span>
-                        <input type="email" required>
-                        <label>Email</label>
-                    </div>
-                    <div class="input-box">
-                        <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
-                        <input type="password" required>
-                        <label>Password</label>
-                    </div>
-                    <div class="remember-forgot">
-                        <label><input type="checkbox">
-                        Remember me</label>
-                        <a href="#">Forgot Password?</a>
-                    </div>
-                    <button type="submit" class="btn">Login</button>
-                    <div class="login-register">
-                        <p>Don't have an account?<a href="#" class="register-link">Register</a></p>
-                    </div>
-                </form>  
-            </div>
-
-            <div class="form-box register">
-                <h2>Registration</h2>
-                <form action="#">
                     <div class="input-box">
                         <span class="icon"><ion-icon name="person"></ion-icon></span>
                         <input type="text" required>
--- backend.php
+++ backend.php
@@ -1,1 +1,26 @@
-<?
+<?php 
+    $conn = mysqli_connect("localhost","root","","resq");
+    if(isset($_POST['submit'])){
+        $user_type = $_POST['user-type'];
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $pass = md5($_POST['password']);
+    
+        $select = " SELECT * FROM resq WHERE email = '$email' && password = '$pass' ";
+    
+        $result = mysqli_query($conn, $select);
+    
+        if(mysqli_num_rows($result) > 0){
+            $error[] = 'user already exists!';
+        }else{
+    
+            if($pass != $)
+    
+        }
+    };
+    if(isset($error)){
+        foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+        };
+    };
+?>
+
